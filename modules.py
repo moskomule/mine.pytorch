252ba908448cbbbce65cb628164788d843a10b9c
moskomule <hataya@keio.jp>
2019-03-22 10:41:08 +0900

    update

--- modules.py
+++ modules.py
@@ -1,13 +1,14 @@
 import math
+
 import torch
 from torch import nn
+from torch.nn import functional as F
 
 
-def pair(x):
-    if len(tuple(x)) == 1:
-        return (x, x)
-    else:
-        return x
+def pair(i):
+    if len(tuple(i)) == 1:
+        return (i, i)
+    return i
 
 
 class BiasConv2d(nn.Module):
@@ -17,21 +18,82 @@ class BiasConv2d(nn.Module):
         >>> conv = BiasConv2d(32, 32, 3)
         >>> conv(torch.randn(32, 32, 28, 28), bias)
         """
-
+        super(BiasConv2d, self).__init__()
         self.kernel_size = pair(kernel_size)
         self.stride = pair(stride)
         self.padding = pair(padding)
         self.dilation = pair(dilation)
-        self.weight = nn.Parameter(torch.Tensor(
-            in_channels, out_channels // groups, *self.kernel_size))
+        self.weight = nn.Parameter(torch.empty(in_channels,
+                                               out_channels // groups,
+                                               *self.kernel_size))
 
     def forward(self, input, bias):
-        return F.conv2d(input, self.weight, bias, self.stride,
-                        self.padding, self.dilation, self.groups)
+        return F.conv2d(input, self.weight, bias, self.stride, self.padding,
+                        self.dilation, self.groups)
 
-    def reset_parameters(self):
-        n = self.in_channels
-        for k in self.kernel_size:
-            n *= k
-        stdv = 1. / math.sqrt(n)
-        self.weight.data.uniform_(-stdv, stdv)
+
+class GaussianNoiseLayer(nn.Module):
+    def __init__(self, std: float = 1):
+        super(GaussianNoiseLayer, self).__init__()
+        self.std = std
+
+    def forward(self, input):
+        return input + torch.empty_like(input).normal_(0, self.std)
+
+
+class ConcatLayer(nn.Module):
+    def __init__(self,
+                 cat_dim=-1):
+        super(ConcatLayer, self).__init__()
+        self.cat_dim = cat_dim
+
+    def forward(self, *input):
+        return torch.cat(input, dim=self.cat_dim)
+
+
+def fc_static_net(x_dim: int,
+                  z_dim: int,
+                  hidden_dim: int = 512):
+    return nn.Sequential(ConcatLayer(),
+                         GaussianNoiseLayer(0.3),
+                         nn.Linear(x_dim + z_dim, hidden_dim),
+                         nn.ELU(),
+                         nn.Linear(hidden_dim, hidden_dim),
+                         nn.ELU(),
+                         nn.Linear(hidden_dim, 1))
+
+
+class ConvStaticNet(nn.Module):
+    def __init__(self, input_size=32):
+        super(ConvStaticNet, self).__init__()
+        self.conv1 = BiasConv2d(3, 16, 5, stride=2, padding=2)
+        self.conv2 = BiasConv2d(16, 32, 5, stride=2, padding=2)
+        self.conv3 = BiasConv2d(32, 64, 5, stride=2, padding=2)
+        self.conv4 = BiasConv2d(64, 128, 5, stride=2, padding=2)
+        self.linear = nn.Linear(input_size // 16, 1)
+
+    def forward(self,
+                input: torch.Tensor,
+                bias: torch.Tensor):
+        x = F.elu(self.conv1(input, bias))
+        x = F.elu(self.conv2(x, bias))
+        x = F.elu(self.conv3(x, bias))
+        x = F.elu(self.conv4(x, bias))
+        return self.linear(F.adaptive_avg_pool2d(x, 1))
+
+
+class MINE(nn.Module):
+    def __init__(self, static_net):
+        super(MINE, self).__init__()
+        self.static_net = static_net
+
+    def forward(self,
+                input: torch.Tensor,
+                target: torch.Tensor) -> torch.Tensor:
+        # returns lower bound of MI
+        # when update, update the negative of the returned value
+        joint = self.static_net(input, target).mean(dim=-1)
+        # -log(size) for averaging
+        margin = self.static_net(input, target[torch.randperm(target.size(0))]).logsumexp(dim=-1) \
+                 - math.log(target.size(0))
+        return joint + margin
