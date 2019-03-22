dfade0a8e0c4f7a60b92af685e48d7c23e89cbf1
moskomule <hataya@keio.jp>
2019-03-22 17:23:20 +0900

    update

--- invariance.py
+++ invariance.py
@@ -6,12 +6,16 @@ from modules import KLMINE, FCStaticNet
 
 
 def normalize(t: torch.Tensor):
-    return t / t.norm(dim=-1, keepdim=True)
+    if args.normalize_input:
+        return t / t.norm(dim=-1, keepdim=True)
+    else:
+        return t
 
 
 def main():
     final_results = {}
     function = {"x": lambda x: x,
+                "2x": lambda x: 2 * x,
                 "x^3": lambda x: x ** 3,
                 "sin(x)": lambda x: x.sin()}[args.function]
     device = "cuda" if torch.cuda.is_available() else "cpu"
@@ -40,7 +44,8 @@ if __name__ == '__main__':
     p.add_int("--epochs", default=10_000)
     p.add_int("--batch_size", default=512)
     p.add_multi_float("--rho", default=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 9.9])
-    p.add_str("--function", choices=["x", "x^3", "sin(x)"])
+    p.add_str("--function", choices=["x", "2x", "x^3", "sin(x)"])
+    p.add_true("--normalize_input")
 
     args = p.parse()
     main()
